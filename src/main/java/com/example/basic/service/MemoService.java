256540702056d14d346e97ddb5ca307bac0adfd4
Park-EJ <sophia5091191@gmail.com>
2025-02-03 17:02:58 +0900

    완료

---
+++ src/main/java/com/example/basic/service/MemoService.java
@@ -0,0 +1,67 @@
+package com.example.basic.service;
+
+import com.example.basic.dto.MemoRequestDto;
+import com.example.basic.dto.MemoResponseDto;
+import com.example.basic.entity.Memo;
+import com.example.basic.repository.MemoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+
+    @Transactional
+    public MemoResponseDto save(MemoRequestDto dto) {
+
+        Memo memo = new Memo(dto.getContent());
+        Memo savedMemo = memoRepository.save(memo);
+        return new MemoResponseDto(savedMemo.getId(), savedMemo.getContent());
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemoResponseDto> findAll() {
+        List<Memo> memos = memoRepository.findAll();
+
+        List<MemoResponseDto> dtos = new ArrayList<>();
+        for (Memo memo : memos) {
+            dtos.add(new MemoResponseDto(memo.getId(), memo.getContent()));
+        }
+
+        return dtos;
+    }
+
+    @Transactional(readOnly = true)
+    public MemoResponseDto findById(Long id) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 id가 존재하지 않습니다.")
+        );
+
+        return new MemoResponseDto(memo.getId(), memo.getContent());
+    }
+
+    @Transactional
+    public MemoResponseDto updateContent(Long id, MemoRequestDto dto) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 id가 존재하지 않습니다.")
+        );
+
+        Memo updatedMemo = memoRepository.updateContent(id, dto.getContent());
+
+        return new MemoResponseDto(updatedMemo.getId(), updatedMemo.getContent());
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당하는 메모가 없습니다.")
+        );
+        memoRepository.deleteById(id);
+    }
+}
